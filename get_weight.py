844f03d7f7855b0cf9c63c23687643ad112a5d0b
ColosseCopie <benji.dream69@gmail.com>
2025-05-26 20:01:35 +0200

    Add files via upload

---
+++ get_weight.py
@@ -0,0 +1,18 @@
+import torch
+
+
+pth_file = r'C:\Users\benja\mast3r\checkpoints\MASt3R_ViTLarge_BaseDecoder_512_catmlpdpt_metric.pth'
+
+checkpoint = torch.load(pth_file, map_location='cpu')
+
+print(checkpoint.keys())
+
+state_dict = checkpoint['model']
+
+qkv_weight = state_dict['enc_blocks.0.attn.qkv.weight']
+
+state_dict['enc_blocks.0.attn.qkv.weight'] = qkv_weight * 0.001
+
+torch.save(checkpoint, r'C:\Users\benja\mast3r\checkpoints\modified_mast3r.pth')
+
+ 
