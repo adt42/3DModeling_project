0340cc5a21cbed6724e5215f76b0a795b274cc71
ColosseCopie <benji.dream69@gmail.com>
2025-05-26 20:03:54 +0200

    Update get_weight.py

--- get_weight.py
+++ get_weight.py
@@ -3,8 +3,8 @@
 
 import torch
 
-
-pth_file = r'C:\Users\benja\mast3r\checkpoints\MASt3R_ViTLarge_BaseDecoder_512_catmlpdpt_metric.pth'
+#The path file
+pth_file = r'C:\Users\benja\mast3r\checkpoints\MASt3R_ViTLarge_BaseDecoder_512_catmlpdpt_metric.pth' 
 
 checkpoint = torch.load(pth_file, map_location='cpu')
 
@@ -14,7 +14,8 @@ state_dict = checkpoint['model']
 
 qkv_weight = state_dict['enc_blocks.0.attn.qkv.weight']
 
-state_dict['enc_blocks.0.attn.qkv.weight'] = qkv_weight * 0.001
+#Here we change the weight
+state_dict['enc_blocks.0.attn.qkv.weight'] = qkv_weight * 1.1
 
 torch.save(checkpoint, r'C:\Users\benja\mast3r\checkpoints\modified_mast3r.pth')
 
